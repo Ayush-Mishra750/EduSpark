e65764ac41bbd06fc50c8165d8a8a83a458f85b2
mishra Ji <ayushmishra270306@gmail.com>
2025-07-10 12:20:35 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div><h2>hii my name is ayush mishra</h2>
+  <Button>click me</Button></div>
+  
+  );
+}
